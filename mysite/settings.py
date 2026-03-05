fa172467acd2ff849457f57d04007969d575b0dc
vismaya14-cmd <vismayamallesh1406@gmail.com>
2026-03-05 22:42:31 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hqymyv)pzb#!5q^1e-vbucgqh%9tntgl-%@v*#@mcy&8&5v2-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
